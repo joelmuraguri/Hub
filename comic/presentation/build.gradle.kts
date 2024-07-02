0846ea48e4d68fc1ab4233d86f21df207de2065e
joelmuraguri <joelkmuraguri21@gmail.com>
2024-07-02 15:05:03 +0300

    add comic presentation module

---
+++ comic/presentation/build.gradle.kts
@@ -0,0 +1,16 @@
+import com.joel.convention.util.ProjectConfig
+
+plugins {
+    alias(libs.plugins.hub.kotlin.android)
+    alias(libs.plugins.hub.android.library)
+    alias(libs.plugins.hub.android.compose)
+}
+
+android {
+    namespace = "com.joel.presentation"
+
+    compileOptions {
+        sourceCompatibility = ProjectConfig.sourceCompatibility
+        targetCompatibility = ProjectConfig.targetCompatibility
+    }
+}
